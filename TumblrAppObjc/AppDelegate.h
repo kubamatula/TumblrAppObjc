830e4ae70ae6b30e283035fb602a91b7468f743e
Jakub Matuła <kubsma@gmail.com>
2017-10-06 15:26:27 +0200

    Initial Commit

---
+++ TumblrAppObjc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TumblrAppObjc
+//
+//  Created by Jakub Matuła on 06/10/2017.
+//  Copyright © 2017 Jakub Matuła. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TumblrAppObjc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TumblrAppObjc
+//
+//  Created by Jakub Matuła on 06/10/2017.
+//  Copyright © 2017 Jakub Matuła. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
